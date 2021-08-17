251d149ef13b797bce04aac4a4bc2aec39e34c5a
jorgelmp <jlzphendrix@gmail.com>
2021-08-17 16:42:38 -0500

    usuarios y utilidad de eliminación

---
+++ Workspace/s-01-usuarios.sql
@@ -0,0 +1,30 @@
+--@Autor: Jesús Salazar Domínguez y Jorge Manzanares Peña 
+--@Fecha de creación: 10/08/2021
+--@Descripción: creación de usuarios y roles
+
+prompt Proporcionar el password del usuario sys;
+
+connect sys as sysdba
+
+show user
+
+prompt Creando usuario ms_proy_admin
+create user ms_proy_admin identified by proyecto quota unlimited on users;
+
+prompt Creando usuario ms_proy_invitado
+create user ms_proy_invitado identified by proyecto;
+
+prompt Creando roles
+create role rol_admin;
+grant create table, create procedure, create sequence,
+ create session, create synonym, create view to proy_admin_rol;
+
+create role rol_invitado;
+grant create session to rol_invitado;
+
+prompt Asignando roles
+grant rol_admin to ms_proy_admin;
+grant rol_invitado to ms_proy_invitado;
+
+prompt Listo!
+disconnect;
---
+++ Workspace/s-504-borrar-todo.sql
@@ -0,0 +1,9 @@
+--@Autor: Jesús Salazar Domínguez y Jorge Manzanares Peña 
+--@Fecha de creación: 10/08/2021
+--@Descripción: Borrar y empezar de nuevo
+
+drop user ms_proy_admin cascade;
+drop user ms_proy_invitado cascade;
+drop role rol_admin;
+drop role rol_invitado;
+
