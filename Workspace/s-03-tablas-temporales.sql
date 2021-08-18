f1609b6576f1549d6838cdd15319a78885713b8f
JesusESD <jesusesd5@gmail.com>
2021-08-17 20:31:26 -0500

    Primera versión de todas las tablas con constraints terminadas

---
+++ Workspace/s-03-tablas-temporales.sql
@@ -0,0 +1,16 @@
+--@Autor: Jesús Salazar Domínguez y Jorge Manzanares Peña 
+--@Fecha de creación: 10/08/2021
+--@Descripción: Definición de tabla temporal
+
+--Un usuario en búsqueda de una casa para vacacionar, puede ir eligiendo
+--sus favoritas y añadirlas a esta tabla a modo de "carrito"
+
+create global temporary table vivienda_carrito_vacaciones_temp(
+  usuario_id    number(10,0),
+  vivienda_id   number(10,0),
+  direccion     varchar2(50),
+  capacidad_max number(3,0),
+  costo_dia     number(5,0),
+  dias_max      number(3,0),
+  deposito      number(6,0)
+) on commit preserve rows;
