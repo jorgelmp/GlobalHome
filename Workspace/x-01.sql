fc7dcf7b7bcae604680574ec1f7c16c80f6cea6e
jorgelmp <jlzphendrix@gmail.com>
2021-08-18 09:54:44 -0500

    Se agregaron encabezados a la utilería de eliminación

--- Workspace/x-01.sql
+++ Workspace/x-01.sql
@@ -1,3 +1,7 @@
+--@Autor: Jesús Salazar Domínguez y Jorge Manzanares Peña 
+--@Fecha de creación: 10/08/2021
+--@Descripción: Utilería para eliminar todas las tablas de un usuario
+
 spool x-02.sql
 select 'drop table '||table_name||' cascade constraints;' from user_tables;
 spool off
