f7ecc43cf2f0061a1aafbd6087c4f2698ef06608
jorgelmp <jlzphendrix@gmail.com>
2021-08-18 14:26:34 -0500

    Se eliminó directorio Workspace

--- Workspace/x-01.sql
+++
@@ -1,9 +0,0 @@
---@Autor: Jesús Salazar Domínguez y Jorge Manzanares Peña 
---@Fecha de creación: 10/08/2021
---@Descripción: Utilería para eliminar todas las tablas de un usuario
-
-spool x-02.sql
-select 'drop table '||table_name||' cascade constraints;' from user_tables;
-spool off
-@x-02.sql
-!rm x-02.sql
