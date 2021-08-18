92ee6a80cdf15dbd92245a62519d52d633423722
jorgelmp <jlzphendrix@gmail.com>
2021-08-18 01:13:59 -0500

    Se agregó un mensaje de creación a la tabla temporal global

--- Workspace/s-03-tablas-temporales.sql
+++ Workspace/s-03-tablas-temporales.sql
@@ -5,6 +5,7 @@
 --Un usuario en búsqueda de una casa para vacacionar, puede ir eligiendo
 --sus favoritas y añadirlas a esta tabla a modo de "carrito"
 
+prompt Creando tabla temporal global
 create global temporary table vivienda_carrito_vacaciones_temp(
   usuario_id    number(10,0),
   vivienda_id   number(10,0),
