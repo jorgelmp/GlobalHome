4e0ea91420374194ff0231a3102cc15297c36c49
jorgelmp <jlzphendrix@gmail.com>
2021-08-18 18:48:33 -0500

    Corrección de errores

--- utilerias/x-01-drop-tabl.sql
+++ utilerias/x-01-drop-tabl.sql
@@ -2,8 +2,8 @@
 --@Fecha de creación: 10/08/2021
 --@Descripción: Utilería para eliminar todas las tablas de un usuario
 
-spool x-02.sql
+spool y-01.sql
 select 'drop table '||table_name||' cascade constraints;' from user_tables;
 spool off
-@x-02.sql
-!rm x-02.sql
+@y-01.sql
+!rm y-01.sql
--- utilerias/x-02-drop-obj.sql
+++ utilerias/x-02-drop-obj.sql
@@ -4,7 +4,7 @@
 
 spool y-02.sql
 select 'drop '||object_type||' '|| object_name || ';' from user_objects 
-where object_type = in ('VIEW','PACKAGE','SEQUENCE', 'PROCEDURE', 'FUNCTION', 'INDEX');
+where object_type = 'SEQUENCE' --in ('VIEW','PACKAGE','SEQUENCE', 'PROCEDURE', 'FUNCTION', 'INDEX');
 spool off
 @y-02.sql
 !rm y-02.sql
