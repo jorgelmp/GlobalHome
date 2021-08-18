cf1ea4c64e8d9a5d8be8f97c397eee323edd906e
JesusESD <jesusesd5@gmail.com>
2021-08-18 16:47:37 -0500

    Inserción del segundo registro (venta)

--- src/s-09-carga-inicial.sql
+++ src/s-09-carga-inicial.sql
@@ -50,8 +50,65 @@ values (seq_servicio.nextval, 'Televisión satelital', 'Canales adicionales
 
 Prompt 7
 insert into status_vivienda (status_vivienda_id, clave, descripcion)
-values (seq_status_vivienda.nextval, 'DI', 'Se asigna a una vivienda cuando 
-está disponible para ser rentada, comprada o alquilada'
+values (seq_status_vivienda.nextval, 'DI', 'DISPONIBLE'
+)
+;
+
+Prompt 7.1
+insert into status_vivienda (status_vivienda_id, clave, descripcion)
+values (seq_status_vivienda.nextval, 'ER', 'EN RENTA'
+)
+;
+
+Prompt 7.2
+insert into status_vivienda (status_vivienda_id, clave, descripcion)
+values (seq_status_vivienda.nextval, 'ALQ', 'ALQUILADA'
+)
+;
+
+Prompt 7.3
+insert into status_vivienda (status_vivienda_id, clave, descripcion)
+values (seq_status_vivienda.nextval, 'EV', 'EN VENTA'
+)
+;
+
+Prompt 7.4
+insert into status_vivienda (status_vivienda_id, clave, descripcion)
+values (seq_status_vivienda.nextval, 'VEN', 'VENDIDA'
+)
+;
+
+Prompt 7.5
+insert into status_vivienda (status_vivienda_id, clave, descripcion)
+values (seq_status_vivienda.nextval, 'IN', 'INACTIVA'
+)
+;
+
+Prompt 7,6
+insert into tarjeta_credito (tarjeta_credito_id, num_tarjeta, mes_expiracion,
+  anio_expiracion, num_seguridad)
+values (seq_tarjeta_credito.nextval, 2528907610987658, 10, 2022, 3493
+)
+;
+
+Prompt 7,7
+insert into tarjeta_credito (tarjeta_credito_id, num_tarjeta, mes_expiracion,
+  anio_expiracion, num_seguridad)
+values (seq_tarjeta_credito.nextval, 8716509248167119, 11, 2023, 1986
+)
+;
+
+Prompt 7,8
+insert into tarjeta_credito (tarjeta_credito_id, num_tarjeta, mes_expiracion,
+  anio_expiracion, num_seguridad)
+values (seq_tarjeta_credito.nextval, 5519036589172536, 12, 2022, 5112
+)
+;
+
+Prompt 7,6
+insert into tarjeta_credito (tarjeta_credito_id, num_tarjeta, mes_expiracion,
+  anio_expiracion, num_seguridad)
+values (seq_tarjeta_credito.nextval, 3252668719052398, 10, 2022, 6071
 )
 ;
 
@@ -63,6 +120,46 @@ values (seq_usuario.nextval, 'HArribasM8@yahoo.com', 'HuguitoA', 'Hugo',
 )
 ;
 
+Prompt 8.1
+insert into usuario (usuario_id, email, nombre_usuario, nombre, ap_paterno,
+  ap_materno, contrasenia)
+values (seq_usuario.nextval, 'Samper_PinM@gmail.com', 'Marco55', 'Marco',
+  'Pineda', 'Samper', '012SamPiM='
+)
+;
+
+Prompt 8.2
+insert into usuario (usuario_id, email, nombre_usuario, nombre, ap_paterno,
+  ap_materno, contrasenia)
+values (seq_usuario.nextval, 'Manu_RieFa@me.com', 'RieraFa1', 'Manuelita',
+  'Riera', 'Fabregat', 'FabRIMAn3210.'
+)
+;
+
+Prompt 8.3
+insert into usuario (usuario_id, email, nombre_usuario, nombre, ap_paterno,
+  ap_materno, contrasenia)
+values (seq_usuario.nextval, 'PugaFiel_11@outlook.com', 'PugaFiel11', 'Fidel',
+  'Puga', 'Piñol', 'PPFcasas/89'
+)
+;
+
+Prompt 8.4
+insert into usuario (usuario_id, email, nombre_usuario, nombre, ap_paterno,
+  ap_materno, contrasenia)
+values (seq_usuario.nextval, 'CarbonellNebot5@me.com', 'NebotCC', 'Nebot',
+  'Conrado', 'Carbonell', 'CarbConNe123'
+)
+;
+
+Prompt 8.5
+insert into usuario (usuario_id, email, nombre_usuario, nombre, ap_paterno,
+  ap_materno, contrasenia)
+values (seq_usuario.nextval, '_RiverCarJon@gmail.com', 'RioCardJo', 'Jonathan',
+  'Río', 'Cárdenas', 'RiversideJonh5'
+)
+;
+
 Prompt 9
 insert into vivienda (vivienda_id, es_renta, es_vacional, es_venta, direccion,
   latitud, longitud, capacidad_max, descripcion, status_vivienda_id, usuario_id)
@@ -70,15 +167,14 @@ values (seq_vivienda.nextval, 1, 0, 0,
   'Avenida Siempre Viva #82, Col. Almizclera, Tlaxcala, México', '23.608°N',
     '100.532°W', 6, 'Casa estilo barroco de dos plantas en muy buena ubicación.
     Vecindario muy seguro con escuelas y plazas comerciales cerca. Terreno de
-    300 metros cuadrados.', seq_status_vivienda.currval, seq_usuario.currval
+    300 metros cuadrados.', 2, 1
 )
 ;
 
 Prompt 10
 insert into historico_status_vivienda (historico_status_vivienda_id,
   vivienda_id, status_vivienda_id)
-values (seq_historico_status_vivienda.nextval, seq_vivienda.currval,
-  seq_status_vivienda.currval
+values (seq_historico_status_vivienda.nextval, seq_vivienda.currval, 2
 )
 ;
 
@@ -133,9 +229,9 @@ Prompt 18
 insert into mensaje (mensaje_id, titulo, cuerpo, leido, usuario_id,
   respuesta_id, vivienda_id)
 values (seq_mensaje.nextval, 'Interés en su casa', 'Buenas tardes 
-  don Antonio, me gustaría comunicarme personalmente con usted para de una 
-  posible renta de su casa, ya que me atrae mucho. Saludos cordiales,
-  Ramiro Segura.',0,seq_usuario.currval,null,seq_vivienda.currval
+  señor, me gustaría comunicarme personalmente con usted para de una 
+  posible renta de su casa, ya que me atrae mucho. Saludos cordiales.',
+  0 ,2, null, seq_vivienda.currval
 )
 ;
 
@@ -158,20 +254,121 @@ values (seq_clabe_vivienda.nextval, seq_vivienda.currval,
 )
 ;
 
-Prompt 22
-insert into usuario (usuario_id, email, nombre_usuario, nombre, ap_paterno,
-  ap_materno, contrasenia)
-values (seq_usuario.nextval, 'Samper_PinM@gmail.com', 'Marco55', 'Marco',
-  'Pineda', 'Samper', '012SamPiM='
-)
-;
-
 Prompt 23
 insert into contrato (contrato_id, folio, documento, vivienda_id,
   usuario_id)
-values(seq_contrato.nextval, 98071652, empty_blob(), seq_vivienda.currval,
-  seq_usuario.currval
+values(seq_contrato.nextval, '9807165B', empty_blob(), seq_vivienda.currval, 2
 )
 ;
 
--- Validado hasta aqu
+-- Validado hasta aquí
+
+Prompt 24
+insert into vivienda (vivienda_id, es_renta, es_vacional, es_venta, direccion,
+  latitud, longitud, capacidad_max, descripcion, status_vivienda_id, usuario_id)
+values (seq_vivienda.nextval, 0, 1, 0,
+  'Andador Bugambilias #12, Col. Morelos, Puebla, México', '21.509°N',
+    '105.982°W', 3, 'Casa humilde amueblada, ideal para un familia recién 
+    formada o pareja sin compromiso. Servicios básicos.', 4, 3
+)
+;
+
+Prompt 25
+insert into historico_status_vivienda (historico_status_vivienda_id,
+  vivienda_id, status_vivienda_id)
+values (seq_historico_status_vivienda.nextval, seq_vivienda.currval, 4
+)
+;
+
+Prompt 26
+insert into imagen (num_foto, vivienda_id, foto)
+values (1, seq_vivienda.currval, empty_blob()
+)
+;
+
+Prompt 27
+insert into vivienda_servicio (vivienda_servicio_id, servicio_id, vivienda_id)
+values (seq_vivienda_servicio.nextval, 1,
+  seq_vivienda.currval
+)
+;
+
+Prompt 28
+insert into vivienda_servicio (vivienda_servicio_id, servicio_id, vivienda_id)
+values (seq_vivienda_servicio.nextval, 2,
+  seq_vivienda.currval
+)
+;
+
+Prompt 29
+insert into vivienda_servicio (vivienda_servicio_id, servicio_id, vivienda_id)
+values (seq_vivienda_servicio.nextval, 3,
+  seq_vivienda.currval
+)
+;
+
+Prompt 30
+insert into mensaje (mensaje_id, titulo, cuerpo, leido, usuario_id,
+  respuesta_id, vivienda_id)
+values (seq_mensaje.nextval, 'Me agardó su humildad', 'Hola, cómo está?
+  Me gustó su casita, me la podría mostrar? Escríbame sí si. Adiós',
+  1, 4, null, 2
+)
+;
+
+Prompt 31
+insert into mensaje (mensaje_id, titulo, cuerpo, leido, usuario_id,
+  respuesta_id, vivienda_id)
+values (seq_mensaje.nextval, 'Cuándo nos vemos?', 'Buenas buenas, si quiere
+  ver la casa podemos vernos el viernes en la tarde o en la noche, por ahí
+  de las 7:30 me viene bien. En la dirección de la casa que viene ahí. 
+  Confírmeme.', 0, 3, null, 2
+)
+;
+
+Prompt 32 Alterar
+update mensaje
+set respuesta_id = seq_mensaje.currval
+where mensaje_id = seq_mensaje.currval - 1
+;
+
+Prompt 33
+insert into vivienda_en_venta (vivienda_id, num_catastral, folio, avaluo)
+values (seq_vivienda.currval, 1178654902156712, '5561901273953760AG',
+  empty_blob()
+)
+;
+
+Prompt 34
+insert into venta_usuario (venta_usuario_id, comision, clabe, vivienda_id,
+  usuario_id)
+values (seq_venta_usuario.nextval, 40000, 781029469281125561, seq_vivienda.currval,
+  4
+)
+;
+
+Prompt 35
+insert into mensualidad (mensualidad_id, fecha_pago, importe, archivo,
+  venta_usuario_id)
+values (seq_mensualidad.nextval, to_date('18/06/2021', 'dd/mm/yyyy'), 50000,
+  empty_blob(), seq_venta_usuario.currval
+)
+;
+
+Prompt 36
+insert into mensualidad (mensualidad_id, fecha_pago, importe, archivo,
+  venta_usuario_id)
+values (seq_mensualidad.nextval, to_date('18/07/2021', 'dd/mm/yyyy'), 40000,
+  empty_blob(), seq_venta_usuario.currval
+)
+;
+
+Prompt 37
+insert into mensualidad (mensualidad_id, importe, archivo,
+  venta_usuario_id)
+values (seq_mensualidad.nextval, 50000, empty_blob(), seq_venta_usuario.currval
+)
+;
+
+commit;
+
