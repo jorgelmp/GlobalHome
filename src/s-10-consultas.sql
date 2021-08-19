91c930e1aacede027b486b0429b46da4da44e847
jorgelmp <jlzphendrix@gmail.com>
2021-08-19 11:50:25 -0500

    Falta validar

--- src/s-10-consultas.sql
+++ src/s-10-consultas.sql
@@ -2,6 +2,8 @@
 --@Fecha de creación: 16/08/2021
 --@Descripción: Consultas
 
+set linesize window
+
 Prompt Consulta para conocer la mensualidad más cara que se ha pagado.
 select vu.vivienda_id, vv.folio , u.nombre, u.ap_paterno , u.ap_materno ,
 m.fecha_pago, m.importe importe_maximo
@@ -10,7 +12,7 @@ where vv.vivienda_id = vu.vivienda_id
 and u.usuario_id = vu.usuario_id 
 and vu.venta_usuario_id = m.venta_usuario_id
 and m.importe = (
-  select max(importe) maximo
+  select max(importe)
   from mensualidad
 )
 ;
@@ -26,5 +28,70 @@ on vr.vivienda_id = cv.vivienda_id
 group by v.vivienda_id, v.direccion, vr.renta_mensual,vr.dia_deposito
 having count(*) > 1
 ;
+
+Prompt Consulta para ver todos los registros históricos desglosados
+select h.historico_status_vivienda_id,h.fecha_status,s.descripcion,v.direccion
+from historico_status_vivienda h
+natural join status_vivienda s
+join vivienda v
+using (vivienda_id)
+order by vivienda_id
+;
+
+
+Prompt Consulta para ver todas las casa en renta y, si tienen, su contrato de renta
+select v.direccion,u.nombre||' '||u.ap_paterno||' '||u.ap_materno arrendador,
+c.folio contrato,c2.nombre||' '||c2.ap_paterno||' '||c2.ap_materno arrendatario
+from casa v, usuario u, vivienda_en_renta vr, contrato c, cliente c2
+where v.usuario_id = u.usuario_id
+and v.vivienda_id = vr.vivienda_id
+and vr.vivienda_id = c.vivienda_id(+)
+and c.usuario_id = c2.usuario_id(+)
+;
+
+select v.vivienda_id, s.nombre
+from vivienda_servicio v
+join servicio s
+on s.servicio_id = v.servicio_id
+;
+
+Prompt Consultar todas las casas que tienen televisión satelital pero no internet
+
+select v.vivienda_id, v.direccion, v.capacidad_max
+from vivienda v
+join vivienda_servicio vs
+on vs.vivienda_id = v.vivienda_id
+join servicio s
+on vs.servicio_id = s.servicio_id
+where s.nombre = 'Electricidad'
+
+minus
+
+select v.vivienda_id, v.direccion, v.capacidad_max
+from vivienda v
+join vivienda_servicio vs
+on vs.vivienda_id = v.vivienda_id
+join servicio s
+on vs.servicio_id = s.servicio_id
+where s.nombre = 'Internet'
+;
+
+prompt Desplegando todos los datos de las tablas disponibles para vacacionar
+select * from v_vivienda_vacacional;
+
+prompt Consulta para la tabla externa
+select * from vivienda_vacacional_ext;
+
+prompt Consulta para la tabla temporal
+select * from vivienda_carrito_vacaciones_temp;
+
+
+
+
+
+
+
+
+
 
 
