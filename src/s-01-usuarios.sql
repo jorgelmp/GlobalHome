9f73be26ee82ded2c90dbe19e80c097e3fc67986
jorgelmp <jlzphendrix@gmail.com>
2021-08-26 17:38:34 -0500

    Se dio una ubicación más adecuada a la tabla externa.

--- src/s-01-usuarios.sql
+++ src/s-01-usuarios.sql
@@ -26,10 +26,10 @@ prompt Asignando roles
 grant rol_admin to ms_proy_admin;
 grant rol_invitado to ms_proy_invitado;
 
-create or replace directory ext_dir as '/tmp/proyecto';
-!mkdir /tmp/proyecto/
-!chmod 777 /tmp/proyecto
-!cp externa/vivienda_vacacional_ext.csv /tmp/proyecto/
+create or replace directory ext_dir as '/usr/share/appdata/oracle-external/';
+!sudo mkdir -p /usr/share/appdata/oracle-external/
+!sudo chown oracle:oinstall /usr/share/appdata/oracle-external/
+!sudo cp externa/vivienda_vacacional_ext.csv /usr/share/appdata/oracle-external/
 grant read, write on directory ext_dir to ms_proy_admin;
 
 prompt Listo!
--- util/x-00-drop-all.sql
+++ util/x-00-drop-all.sql
@@ -7,6 +7,6 @@ drop user ms_proy_admin cascade;
 drop user ms_proy_invitado cascade;
 drop role rol_admin;
 drop role rol_invitado;
-
+drop directory ext_dir;
 prompt Listo
 
